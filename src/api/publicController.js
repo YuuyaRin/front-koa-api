2cdc229b6c037e7d2e8ce6359697e8c5a68bc2bd
YuuyaRin <yuanye_job@126.com>
2020-03-23 14:49:55 +0800

    update

--- src/api/publicController.js
+++ src/api/publicController.js
@@ -1,6 +1,6 @@
 import svgCaptcha from "svg-captcha";
 class PublicController {
-  constructor() {}
+  constructor() { }
   async getCaptcha(ctx) {
     const newCaptcha = svgCaptcha.create({
       size: 4,
@@ -8,7 +8,7 @@ class PublicController {
       color: true,
       noise: Math.floor(Math.random() * 5),
       width: 150,
-      height: 50
+      height: 38
     });
     ctx.body = {
       code: 200,
